891258ed0037df66708b19e5f8185da4c30b907b
AbeeraUmair <147932381+AbeeraUmair@users.noreply.github.com>
2024-11-13 02:31:15 +0500

    Update main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-function menuToggle() {
-    const menu = document.getElementById("sidebar");
-    if (sidebar.style.display === "none" || menu.style.display === "") {
-        sidebar.style.display = "block"; // Show the menu
-    } else {
-        sidebar.style.display = "none"; // Hide the menu
-    }
-}
